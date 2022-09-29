87c654c83ea6565a43ee36abb34c8844ec9fba67
Kyle McKell <me@kylemckell.com>
2022-09-29 08:35:04 -0600

    making myself happier through formatting

--- .config/nvim/after/plugin/mason.lua
+++ .config/nvim/after/plugin/mason.lua
@@ -1,6 +1,6 @@
 local mason = require('mason')
-local masontool = require('mason-tool-installer')
 local masonlsp = require('mason-lspconfig')
+local masontool = require('mason-tool-installer')
 
 local servers = { 'sumneko_lua', 'tsserver', 'gopls', 'pyright', 'rust_analyzer', 'tailwindcss', 'stylua' }
 local tools = { 'stylua' }
@@ -15,10 +15,10 @@ mason.setup({
   },
 })
 
-masontool.setup({
-  ensure_installed = tools,
-})
-
 masonlsp.setup({
   ensure_installed = servers,
+})
+
+masontool.setup({
+  ensure_installed = tools,
 })
