54bcd516d8db0cb54d4cc4f2bec580e46e04db24
Kyle McKell <me@kylemckell.com>
2022-09-29 11:53:12 -0600

    removed extra setup handlers lmao

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -16,17 +16,6 @@ local on_attach = function(client, bufnr)
   end
 end
 
-masonnulls.setup_handlers({
-  function(source_name) end,
-  stylua = function() nulls.register(nulls.builtins.formatting.stylua) end,
-  jq = function() nulls.register(nulls.builtins.formatting.jq) end,
-  eslint_d = function()
-    nulls.register(nulls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})',
-    }))
-  end,
-})
-
 nvim_lsp.tsserver.setup({
   on_attach = on_attach,
   filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
