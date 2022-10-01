836f2dc46c90ffd4914d84bf3800f093cdf60a18
Kyle McKell <me@kylemckell.com>
2022-10-01 15:40:55 -0600

    gave clarity comments

--- .config/nvim/lua/kylemckell/commands.lua
+++ .config/nvim/lua/kylemckell/commands.lua
@@ -1,4 +1,4 @@
 vim.cmd([[set signcolumn=yes:1]]) -- keeps column on left for symbols
 vim.cmd([[set clipboard+=unnamedplus]]) -- accesses system clipboard for windows
-vim.cmd([[set ls=0]])
-vim.cmd([[set ch=0]])
+vim.cmd([[set ls=0]]) -- last status
+vim.cmd([[set ch=0]]) -- command height
